e5cc8d7fd4627a8dedfb5594b367f73b44a1448e
AhmetYavuzCakmas <116893962+AhmetYavuzCakmas@users.noreply.github.com>
2026-02-25 12:42:02 +0300

    Add files via upload

---
+++ udemy_16_days/aa.py
@@ -0,0 +1,18 @@
+"""class User:
+    def __init__ (self,user_id,user_name):
+        self.id = user_id
+        self.name = user_name
+
+user1 = User("121","ali")
+print(user1.name)
+"""
+from turtle import Turtle.Screen
+screen.Screen()
+screen.setup(600,600)
+screen.bgcolor("black")
+
+
+
+
+
+screen.exitonclick()
